8a0baeea6a6389ccebc755d245f034796b205fd3
Josias Alvarado <josiasjag@gmail.com>
2021-03-09 15:41:49 -0600

    Add get method for MovieDetailView

--- moviesapp/movies/views.py
+++ moviesapp/movies/views.py
@@ -7,23 +7,30 @@ from django.contrib import messages
 from django.shortcuts import redirect
 from django.http import Http404
 from django.urls import reverse_lazy
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core import serializers
 
 from .models import Movie
 
 
 class MovieListView(ListView):
     """Show all movies."""
-
-    model = Movie
+    def get(self, request, *args, **kwargs):
+        queryset = Movie.objects.all()
+        queryset_json = serializers.serialize('json', queryset)
+        return HttpResponse(queryset_json, content_type='application/json')
 
 
 class MovieDetailView(DetailView):
     """Show the requested movie."""
-
+    def get(self, request, isbn, *args, **kwargs):
+        query = Movie.objects.get(pk=isbn)
+        return JsonResponse(query, encoder=ExtendedEncoder, safe=False)
 
 class MovieCreateView(CreateView):
     """Create a new movie."""
-
+    
 
 class MovieUpdateView(UpdateView):
     """Update the requested movie."""
