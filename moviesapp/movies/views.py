87a453c0afa428d73483c8e3197e41af6044fa4e
Josias Alvarado <josiasjag@gmail.com>
2021-03-10 14:53:50 -0600

    Update param name from isbn to pk and add Delete and Update view code

--- moviesapp/movies/views.py
+++ moviesapp/movies/views.py
@@ -2,13 +2,12 @@
 
 """Movies views."""
 
-from django.views.generic import UpdateView, DeleteView
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.core import serializers
 
 from rest_framework.views import APIView
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework_api_key.permissions import HasAPIKey
 
 
@@ -29,19 +28,25 @@ class MovieListView(APIView):
 class MovieDetailView(APIView):
     """Show the requested movie."""
     permission_classes = [HasAPIKey]
-    def get(self, request, isbn):
-        query = Movie.objects.get(pk=isbn)
+    def get(self, request, pk):
+        query = Movie.objects.get(pk=pk)
         return JsonResponse(query, encoder=ExtendedEncoder, safe=False)
 
 
 class MovieCreateView(CreateAPIView):
     """Create a new movie."""
+    permission_classes = [HasAPIKey]
     serializer_class = MovieSerializer
 
 
-class MovieUpdateView(UpdateView):
+class MovieUpdateView(UpdateAPIView):
     """Update the requested movie."""
+    permission_classes = [HasAPIKey]
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
 
 
-class MovieDeleteView(DeleteView):
+class MovieDeleteView(DestroyAPIView):
     """Delete the requested movie."""
+    permission_classes = [HasAPIKey]
+    queryset = Movie.objects.all()
